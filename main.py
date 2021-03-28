5fc397b55b7b63879e9a4aefdf6cf28d80012e07
Nafiz Islam <nafiz.islam1001@gmail.com>
2021-03-28 17:59:00 -0400

    add space after !cowsay

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('!cowsay'):
+    if message.content.startswith('!cowsay '):
         content = message.content[len('!cowsay '):]
         await message.channel.send('```\n' + cowsay.cow(content) + '\n```')
 
