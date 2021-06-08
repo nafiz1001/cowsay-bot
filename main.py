7814f3310e9eaf41574b6f3d9a1a18442b25d8d4
nafiz1001 <nafiz.islam1001@gmail.com>
2021-06-08 18:10:59 -0400

    support multi animal say

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import discord
-import os
 import cowsay
+
+import os
+import re
+import random
 
 client = discord.Client()
 
@@ -13,8 +16,17 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('!cowsay '):
-        content = message.content[len('!cowsay '):]
-        await message.channel.send('```\n' + cowsay.cow(content) + '\n```')
+    try:
+        if match := re.search('^!([^ ]+)say (.*)', message.content.strip()):
+            animal = match[1].lower()
+            response = match[2]
+
+            if animal not in cowsay.char_names:
+                response = f'You wanted "{animal}" but only the following animals exist: {", ".join(cowsay.char_names)}'
+                animal = random.choice(list(cowsay.char_names))
+
+            await message.channel.send(f'```\n{cowsay.get_output_string(animal, response)}\n```')
+    except Exception as e:
+        await message.channel.send(f'{message.author} said {message.content}, but:\n{e}')
 
 client.run(os.getenv('TOKEN'))
