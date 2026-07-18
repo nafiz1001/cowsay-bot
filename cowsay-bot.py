07a0fa9659076bb2f310c3986a43d1e3374b379c
Nafiz Islam <nafiz.islam1000@gmail.com>
2026-07-18 17:11:04 -0400

    large tweaks

--- cowsay-bot.py
+++ cowsay-bot.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
+import asyncio
 import datetime
 import os
+import signal
 
 import cowsay # pyright: ignore[reportMissingTypeStubs]
 import discord
@@ -17,15 +19,6 @@ MESSAGE_LENGTH_MAX = 2000
 intents = discord.Intents.default()
 client = discord.Client(intents=intents)
 tree = discord.app_commands.CommandTree(client)
-
-def get_cowsay_for_discord(character: str, say: str, backoff: int = 1) -> str:
-    message = f"```\n{cowsay.get_output_string(character, say)}\n```"
-    if len(message) <= MESSAGE_LENGTH_MAX:
-        return message
-
-    truncate_by = len(message) - MESSAGE_LENGTH_MAX
-    say = say[:-1 - truncate_by - len("...") - backoff] + "..."
-    return get_cowsay_for_discord(character, say, backoff*2)
 
 @tree.command(
     name="cowsay",
@@ -46,6 +39,23 @@ async def _cowsay(
 
     await interaction.response.send_message(get_cowsay_for_discord(character, text))
 
+def get_cowsay_for_discord(character: str, say: str, backoff: int = 0) -> str:
+    message = f"```\n{cowsay.get_output_string(character, say)}\n```"
+    if len(message) <= MESSAGE_LENGTH_MAX:
+        return message
+
+    truncate_by = len(message) - MESSAGE_LENGTH_MAX
+    say = say[:-1 - truncate_by - backoff]
+
+    if not say:
+        character = "cow"
+        say = (
+            "Sorry. Your message plus the character might have gone past the discord's "
+            "max message limit of 2000 characters 😔..."
+        )
+
+    return get_cowsay_for_discord(character, say, max(backoff, 1) * 2)
+
 @_cowsay.autocomplete("character")
 async def character_autocomplete(
         interaction: discord.Interaction,
@@ -56,12 +66,21 @@ async def character_autocomplete(
         discord.app_commands.Choice(name=char, value=char)
         for char in cowsay.char_names if current in char
     ][:25]
-    
+
+async def setup_hook():
+    def signal_handler():
+        print(f"Cowsay is shutting down!")
+        asyncio.create_task(client.close())
+    loop = asyncio.get_running_loop()
+    loop.add_signal_handler(signal.SIGINT, signal_handler)
+    loop.add_signal_handler(signal.SIGTERM, signal_handler)
+
+    for guild in GUILDS:
+        await tree.sync(guild=guild)
+client.setup_hook = setup_hook
 
 @client.event
 async def on_ready():
-    for guild in GUILDS:
-        await tree.sync(guild=guild)
-    cowsay.cow(f"I'm ready! GUILD_IDS={GUILD_IDS}") # pyright: ignore[reportUnknownMemberType, reportAttributeAccessIssue]
+    print(f"Cowsay is ready!")
 
 client.run(TOKEN)
